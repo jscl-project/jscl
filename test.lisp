95108b1375281d198995c902c700a66b7bc9c6a3
David Vazquez <davazp@gmail.com>
2012-12-16 00:02:49 +0000

    Define defmacro

--- test.lisp
+++ test.lisp
@@ -34,3 +34,17 @@
 (setq x 0)
 (incf x)
 (debug x)
+
+(eval-when-compile
+  (%compile-defmacro 'defmacro
+                     (lambda (name args &rest body)
+                       (list 'eval-when-compile
+                             (list '%compile-defmacro (list 'quote name)
+                                   (list* 'lambda args body))))))
+
+(defmacro %incf (x)
+  (list 'setq x (list '+ 1 x)))
+
+(setq x 10)
+(%incf x)
+(debug x)
