623000fd16dc784cb9e6b5e264f1fbd6eb455d5f
David Vazquez <davazp@gmail.com>
2012-12-17 13:32:43 +0000

    Move library to lispstrack.lisp

--- test.lisp
+++ test.lisp
@@ -1,55 +1,5 @@
 ;;; Library
 
-(eval-when-compile
-  (%compile-defmacro 'defmacro
-     (lambda (name args &rest body)
-       `(eval-when-compile
-          (%compile-defmacro ',name (lambda ,args ,@body))))))
-
-(defmacro defvar (name value)
-  `(progn
-     (eval-when-compile
-       (%compile-defvar ',name))
-     (setq ,name ,value)))
-
-(defmacro defun (name args &rest body)
-  `(progn
-     (eval-when-compile
-       (%compile-defun ',name))
-     (fsetq ,name (lambda ,args ,@body))))
-
-(defun = (x y) (= x y))
-(defun + (x y) (+ x y))
-(defun - (x y) (- x y))
-(defun * (x y) (* x y))
-(defun / (x y) (/ x y))
-(defun 1+ (x) (+ x 1))
-(defun 1- (x) (- x 1))
-(defun cons (x y ) (cons x y))
-(defun car (x) (car x))
-(defun cdr (x) (cdr x))
-
-(defun append (list1 list2)
-  (if (null list1)
-      list2
-      (cons (car list1)
-            (append (cdr list1) list2))))
-
-(defun reverse-aux (list acc)
-  (if (null list)
-      acc
-      (reverse-aux (cdr list) (cons (car list) acc))))
-
-(defun reverse (list)
-  (reverse-aux list '()))
-
-(defun mapcar (func list)
-  (if (null list)
-      '()
-      (cons (funcall func (car list))
-            (mapcar func (cdr list)))))
-
-
 ;;; Tests
 
 (lambda (x y) x)
