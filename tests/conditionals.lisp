604ea926e93002c3270b5669c2db8655488b8769
Strigoides <Strigoides@gmail.com>
2013-04-27 00:41:16 +1200

    Test for DO*, OR, AND & COND

---
+++ tests/conditionals.lisp
@@ -0,0 +1,13 @@
+; Tests for conditional forms
+; Boolean operators
+(test (eql (and nil 1) nil))
+(test (=   (and 1   2)   2))
+
+(test (= (or  nil 1)   1))
+(test (= (or  1   2)   1))
+
+; COND
+(test (eql nil (cond)))
+(test (=   2   (cond (1 2))))
+(test (=   3   (cond (nil 1) (2 3))))
+(test (eql nil (cond (nil 1) (nil 2))))
