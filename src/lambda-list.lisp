84897abb2163b0de6d858267ade2fe06f3c3d946
David Vázquez <davazp@gmail.com>
2013-05-06 00:31:28 +0100

    Add copyright notice

--- src/lambda-list.lisp
+++ src/lambda-list.lisp
@@ -1,5 +1,7 @@
 ;;; lambda-list.lisp --- Lambda list parsing and destructuring
 
+;;; Copyright (C) 2013 David Vazquez
+
 ;; JSCL is free software: you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
 ;; published by the Free Software Foundation, either version 3 of the
