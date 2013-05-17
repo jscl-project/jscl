c3b19b129a11f61ad6bbe17c04b6bff92c048f17
Olof-Joachim Frahm <olof@macrolet.net>
2013-05-18 00:47:05 +0200

    Add and export PUSHNEW.

--- src/toplevel.lisp
+++ src/toplevel.lisp
@@ -70,7 +70,7 @@
           multiple-value-prog1 nconc nil ninth not nreconc nth nthcdr null
           numberp or otherwise package-name package-use-list packagep pairlis
           parse-integer plusp pop prin1-to-string print proclaim prog prog1
-          prog2 progn psetq push quote rassoc read-from-string remove remove-if
+          prog2 progn psetq push pushnew quote rassoc read-from-string remove remove-if
           remove-if-not return return-from revappend reverse rplaca rplacd
           second set setf setq seventh sixth some string string-upcase string=
           string< stringp subseq subst symbol-function symbol-name symbol-package
