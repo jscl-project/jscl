d2827f55d5ee657b9aada76d1e79d8dc9cf07eec
Raimon Grau <raimonster@gmail.com>
2013-05-18 18:12:49 +0200

    tests for vectorp and elt

--- tests/seq.lisp
+++ tests/seq.lisp
@@ -11,7 +11,7 @@
 
 ;; TODO: Rewrite these tests when EQUALP exists and works on vectors
 (let ((v1 (remove-if #'zerop #(1 0 2 0 3))))
-  (test (and (= (aref v1 0) 1) (= (aref v1 1) 2) (= (aref v1 2) 3)))) 
+  (test (and (= (aref v1 0) 1) (= (aref v1 1) 2) (= (aref v1 2) 3))))
 (test (every #'zerop (remove-if-not #'zerop #(1 0 2 0 3))))
 
 (let ((nums '(1 2 3 4 5)))
@@ -19,3 +19,8 @@
   (test (equal (subseq nums 2 4) '(3 4)))
   ; Test that nums hasn't been altered: SUBSEQ should construct fresh lists
   (test (equal nums '(1 2 3 4 5))))
+
+(test (eq 1 (elt (vector '(1 2 3)) 0)))
+(test (eq 1 (elt '(1 2 3) 0)))
+(test (arrayp (vector '(1 2))))
+(test (not (arrayp (vector (list 1 2)))))
