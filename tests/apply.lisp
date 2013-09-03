6fad19d5f281ac1366375384875d542c222a100a
Olof-Joachim Frahm <olof@macrolet.net>
2013-09-03 09:35:23 +0200

    Add FUNCALL, APPLY tests.

---
+++ tests/apply.lisp
@@ -0,0 +1,10 @@
+; Tests for funcall/apply
+
+(test (equal (funcall #'list 1 2 3) '(1 2 3)))
+(test (equal (apply #'list 1 2 3 '(4 5 6)) '(1 2 3 4 5 6)))
+
+(test (equal (funcall #'funcall #'list 1 2 3) '(1 2 3)))
+(test (equal (funcall #'apply #'list 1 2 3 '(4 5 6)) '(1 2 3 4 5 6)))
+
+(test (equal (apply #'funcall #'list 1 2 3 '(4 5 6)) '(1 2 3 4 5 6)))
+(test (equal (apply #'apply #'list 1 2 3 '(4 5 (6))) '(1 2 3 4 5 6)))
