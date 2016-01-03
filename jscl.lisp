86f756d9c1fc88072e96709367a89b5da05407a8
David Vazquez <davazp@gmail.com>
2016-01-03 15:52:22 +0100

    Don't dump the literal table
    The literal table was dumped, so future reference to symbols could be
    found. However, now we have proper packages and we don't need the
    literal table anymore. Symbols will be interned when required.
    
    We still use the literal table for conses and shorter references to
    symbols during bootstraping.
    
    This changes also makes the output file smaller.

--- jscl.lisp
+++ jscl.lisp
@@ -145,7 +145,6 @@
     ;; not collide with the compiler itself.
     (late-compile
      `(progn
-        (setq *literal-table* ',*literal-table*)
         (setq *variable-counter* ,*variable-counter*)
         (setq *gensym-counter* ,*gensym-counter*)))
     (late-compile `(setq *literal-counter* ,*literal-counter*))))
