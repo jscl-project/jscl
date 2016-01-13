18c66e89591cd6ed74becdd259d79d66fd97d7ea
David Vazquez <davazp@gmail.com>
2016-01-13 18:56:53 +0100

    Log the loading of setf.lisp

--- src/setf.lisp
+++ src/setf.lisp
@@ -13,6 +13,7 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with JSCL.  If not, see <http://www.gnu.org/licenses/>.
 
+(/debug "loading setf!")
 
 ;;; Generalized references (SETF)
 
