0c2d935e3825deb7bc3d420a6d08a204cbcb734e
David Vazquez <davazp@gmail.com>
2016-01-19 00:06:05 +0100

    Fix %js-try builtin form

--- src/compiler/compiler.lisp
+++ src/compiler/compiler.lisp
@@ -1493,7 +1493,7 @@
                   ,(convert-block body))))))
 
     (emit `(selfcall
-            (try ,(convert-block form t))
+            (try ,(convert-block (list form) t))
             ,catch-compilation
             ,finally-compilation)
           t)))
