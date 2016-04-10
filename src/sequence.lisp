094636284591f7dbeb56e0a18ddb4047c45373c5
Diogo Franco <diogoalexfranco@gmail.com>
2016-04-10 21:16:51 +0100

    sequence.lisp: implement count-if-not

--- src/sequence.lisp
+++ src/sequence.lisp
@@ -105,6 +105,10 @@
           (incf result))))
     result))
 
+(defun count-if-not (predicate sequence &key from-end (start 0) end key)
+  (count-if (complement predicate) sequence :from-end from-end
+            :start start :end end :key key))
+
 (defun find (item seq &key key (test #'eql testp) (test-not #'eql test-not-p))
   (do-sequence (x seq)
     (when (satisfies-test-p item x :key key :test test :testp testp
