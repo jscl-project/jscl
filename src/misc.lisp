27fec0aa8e003005dec29b2ab38009e1a856f504
Bruce-Robert Fenn Pocock <brpocock@star-hope.org>
2016-08-12 18:08:11 -0400

    null-if-empty

--- src/misc.lisp
+++ src/misc.lisp
@@ -26,43 +26,46 @@
 ;; want to  provide alternative  implementations that  are more  in line
 ;; with what a “normal” compiler would provide.
 
+(defun null-if-empty (x)
+  (if (and x (emptyp x)) nil x))
+
 (defun short-site-name ()
-  (or (and #j:window #j:window:location #j:window:location:hostname)
-      (and #j:os #j:os:hostname (#j:os:hostname))))
+  (null-if-empty (or (and #j:window #j:window:location #j:window:location:hostname)
+                     (and #j:os #j:os:hostname (#j:os:hostname)))))
 
 (defun long-site-name ()
-  (or (and #j:window #j:window:location #j:window:location:origin)
-      (and #j:os #j:os:hostname (#j:os:hostname))))
+  (null-if-empty (or (and #j:window #j:window:location #j:window:location:origin)
+                     (and #j:os #j:os:hostname (#j:os:hostname)))))
 
 (defun machine-instance ()
-  (or (and #j:window #j:window:location #j:window:location:hostname)
-      (and #j:os #j:os:hostname (#j:os:hostname))))
+  (null-if-empty (or (and #j:window #j:window:location #j:window:location:hostname)
+                     (and #j:os #j:os:hostname (#j:os:hostname)))))
 
 (defun machine-version ()
   "The platform or OS type"
-  (let ((platform (or (and #j:navigator #j:navigator:platform)
-                      (and #j:process #j:process:platform))))
-    (if (and platform (find #\Space platform))
-        (subseq platform 0 (position #\Space platform))
-        platform)))
+  (null-if-empty (let ((platform (or (and #j:navigator #j:navigator:platform)
+                                     (and #j:process #j:process:platform))))
+                   (if (and platform (find #\Space platform))
+                       (subseq platform 0 (position #\Space platform))
+                       platform))))
 
 (defun machine-type ()
   "Probably a CPU type"
-  (let ((platform (or (and #j:navigator #j:navigator:platform)
-                      (and #j:process #j:process:arch))))
-    (if (and platform (find #\Space platform))
-        (subseq platform (1+ (position #\Space platform)))
-        platform)))
+  (null-if-empty (let ((platform (or (and #j:navigator #j:navigator:platform)
+                                     (and #j:process #j:process:arch)))))
+                 (if (and platform (find #\Space platform))
+                     (subseq platform (1+ (position #\Space platform)))
+                     platform)))
 
 (defun software-type ()
   "The browser's Product name; eg, Gecko for Firefox"
-  (or (and #j:navigator #j:navigator:product)
-      (and #j:process #j:process:title)))
+  (null-if-empty (or (and #j:navigator #j:navigator:product)
+                     (and #j:process #j:process:title))))
 
 (defun software-version ()
   "The User-Agent string provided by the browser, or Node.js version"
-  (or (and #j:navigator #j:navigator:userAgent)
-      (and #j:process #j:process:version)))
+  (null-if-empty (or (and #j:navigator #j:navigator:userAgent)
+                     (and #j:process #j:process:version))))
 
 (defmacro time (form)
   (let ((start (gensym))
