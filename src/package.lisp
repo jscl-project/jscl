10623083a4156e1662ce7d735ae8bd41de5b4acf
Bruce-Robert Fenn Pocock <brfennpocock@star-hope.org>
2016-10-15 16:52:32 -0400

    (defpackage (:export …))

--- src/package.lisp
+++ src/package.lisp
@@ -99,13 +99,12 @@
          (setf exports (append exports (cdr option))))))
     `(progn
        (eval-when (:load-toplevel :execute)
-         (%defpackage ',(string package) ',use))
+         (%defpackage ',(string package) ',use)
+         ,@(mapcar (lambda (symbol)
+                     `(export (intern ,(string symbol) (find-package ,(string package)))))
+                   exports))
        (eval-when (:compile-toplevel)
-         (make-package ',(string package) :use ',use))
-       ,@(mapcar (lambda (symbol)
-                   (export (intern (symbol-name symbol) package)))
-                 exports))))
-
+         (make-package ',(string package) :use ',use)))))
 
 (defun redefine-package (package use)
   (setf (oget package "use") use)
@@ -117,7 +116,6 @@
     (if package
         (redefine-package package use)
         (make-package name :use use))))
-
 
 (defun find-symbol (name &optional (package *package*))
   (let* ((package (find-package-or-fail package))
@@ -133,7 +131,6 @@
          (let ((exports (%package-external-symbols used)))
            (when (in name exports)
              (return (values (oget exports name) :inherit)))))))))
-
 
 ;;; It is a function to call when a symbol is interned. The function
 ;;; is invoked with the already interned symbol as argument.
