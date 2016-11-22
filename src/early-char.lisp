8b714ba9fcf552ee82626751573314727fcf75ba
Bruce-Robert Fenn Pocock <brpocock@star-hope.org>
2016-11-22 15:12:44 -0500

    remove dependance upon CHECK-TYPE
    Now that types.lisp is coming into play, CHECK-TYPE
    will not yet be available here, but that is only a small loss.

--- src/early-char.lisp
+++ src/early-char.lisp
@@ -145,8 +145,12 @@
 ;; From comment #4 on <https://bugs.launchpad.net/sbcl/+bug/1177986>:
 (defun digit-char-p (char &optional (radix 10))
   "Includes ASCII 0-9 a-z A-Z, plus any Unicode decimal digit characters or fullwidth variants A-Z."
-  (check-type char character)
-  (check-type radix integer)
+  ;; Check-Type not available yet.
+  ;; This could maybe move to char.lisp though?
+  (unless (and (characterp char)
+               (integerp radix)
+               (<= 2 radix 36))
+    (error "Type error"))
   (let* ((radix (or (and radix (<= 2 radix 36) radix) 10))
          (number (unicode-digit-value char))
          (code (char-code char))
