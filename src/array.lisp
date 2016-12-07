5ce2e30563ea7303621aa396d8f81c691e162e1a
Bruce-Robert Fenn Pocock <brpocock@star-hope.org>
2016-12-07 15:25:25 -0500

    JSCL/CL; VECTOR-PUSH-EXTEND fixes
    Move to JSCL/CL package
    
    A few  fixes (untested) for  VECTOR-PUSH-EXTEND to  get a bit  closer to
    ANSI requirements

--- src/array.lisp
+++ src/array.lisp
@@ -1,4 +1,4 @@
-;;; arrays.lisp
+;;; array.lisp
 
 ;; JSCL is free software: you can redistribute it and/or modify it under
 ;; the terms of the GNU General  Public License as published by the Free
@@ -13,35 +13,37 @@
 ;; You should  have received a  copy of  the GNU General  Public License
 ;; along with JSCL. If not, see <http://www.gnu.org/licenses/>.
 
-(in-package :jscl) #-jscl-xc #.(error "Do not load this file in the host compiler")
+(in-package :jscl)
 
 (/debug "loading array.lisp!")
 
-(defun upgraded-array-element-type (typespec &optional environment)
+(defun jscl/cl::upgraded-array-element-type (typespec &optional environment)
   (declare (ignore environment))
   (if (eq typespec 'character)
       'character
       t))
 
-(defun array-dimensions (array)
+(defun jscl/cl::array-dimensions (array)
   (check-type array array)
   (third (storage-vector-kind array)))
-(defun array-element-type (array)
+(defun jscl/cl::array-element-type (array)
   (check-type array array)
   (second (storage-vector-kind array)))
-(defun adjustable-array-p (array)
+(defun jscl/cl::adjustable-array-p (array)
   (check-type array array)
   (fourth (storage-vector-kind array)))
-(defun fill-pointer (array)
+(defun jscl/cl::fill-pointer (array)
   (check-type array array)
   (fifth (storage-vector-kind array)))
-(defun (setf fill-pointer) (new-index array)
+(defun (setf jscl/cl::fill-pointer) (new-index array)
   (check-type array array)
   (setf (fifth (storage-vector-kind array)) new-index))
 
-(defun make-array (dimensions &key element-type initial-element adjustable fill-pointer)
+(defun jscl/cl::make-array (dimensions
+                            &key element-type initial-element 
+                                 adjustable fill-pointer)
   (let* ((dimensions (ensure-list dimensions))
-         (size (!reduce #'* dimensions 1))
+         (size (reduce #'* dimensions 1))
          (array (make-storage-vector
                  size
                  (list 'array element-type dimensions
@@ -49,8 +51,8 @@
     ;; Upgrade type
     (if (eq element-type 'character)
         (progn
-          (oset 1 array "stringp")
-          (setf element-type 'character
+          (setf (jscl/ffi:oget array "stringp") 1
+                element-type 'character
                 initial-element (or initial-element #\space)))
         (setf element-type t))
     ;; Initialize array
@@ -62,54 +64,60 @@
     array))
 
 
-(defun arrayp (x)
+(defun jscl/cl::arrayp (x)
   (and (storage-vector-p x)
        (eql 'array (car (storage-vector-kind x)))))
 
-(defun array-element-type (array)
+(defun jscl/cl::array-element-type (array)
   (unless (arrayp array)
     (error "~S is not an array." array))
   (if (eq (jscl/ffi:oget array "stringp") 1)
       'character
       (jscl/ffi:oget array "type")))
 
-(defun array-dimensions (array)
+(defun jscl/cl::array-dimensions (array)
   (unless (arrayp array)
     (error "~S is not an array." array))
   (jscl/ffi:oget array "dimensions"))
 
 ;; TODO: Error checking
-(defun array-dimension (array axis)
+(defun jscl/cl::array-dimension (array axis)
   (nth axis (array-dimensions array)))
 
-(defun aref (array index)
+(defun jscl/cl::aref (array index)
   (check-type array array)
-  (check-type index (fixnum 0 *))
+  (check-type index (and fixnum (integer 0 *)))
   (storage-vector-ref array index))
 
-(defun aset (array index value)
+(defun jscl/cl::aset (array index value)
   (check-type array array)
-  (check-type index (fixnum 0 *))
+  (check-type index (and fixnum (integer 0 *)))
   (storage-vector-set array index value))
 
-(defun (setf aref) (value array index)
+(defun (setf jscl/cl::aref) (value array index)
   (aset array index value))
 
 
 ;;; Vectors
 
-(defun vectorp (x)
-  (and (arrayp x) (null (cdr (array-dimensions x)))))
+(defun jscl/cl::vectorp (x)
+  (and (jscl/cl::arrayp x) (null (cdr (array-dimensions x)))))
 
-(defun vector (&rest objects)
+(defun jscl/cl::vector (&rest objects)
   (list-to-vector objects))
 
-;;; FIXME:  should  take  optional   min-extension.  FIXME:  should  use
-;;; fill-pointer instead of the absolute end of array
-(defun vector-push-extend (new vector)
-  (unless (vectorp vector)
-    (error "~S is not a vector." vector))
-  (let ((size (storage-vector-size vector)))
-    (resize-storage-vector vector (1+ size))
-    (aset vector size new)
-    size))
+(defun jscl/cl::vector-push-extend (new vector &key min-extension)
+  (unless (jscl/cl::vectorp vector)
+    (error "~S is not a vector." vector)) 
+  (let ((pointer (fill-pointer vector))
+        (size (storage-vector-size vector)))
+    (when (>= pointer size)
+      (resize-storage-vector vector (or min-extension
+                                        (max 1 (min #x100
+                                                    (round size 4))))))
+    (aset vector (incf (fill-pointer vector)) new)
+    (progn
+      ;; with no fill-pointer, only increase by one always. Is that correct?
+      (resize-storage-vector vector (1+ size))
+      (aset vector size new)
+      size)))
