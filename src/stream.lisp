7ff6306a19c0cd5daed581449948bee4b6fddb52
Bruce-Robert Fenn Pocock <brpocock@star-hope.org>
2016-12-07 17:24:55 -0500

    JSCL/CL and fake generic functions
    Paving the  way for Gray  streams implementation using some  very shoddy
    fake generic functions.
    
    Actually, this is more like a  C++ “virtual dispatch table” (vtbl) here,
    but it should™  pave the way for proper Gray  streams once DefGeneric is
    in place.

--- src/stream.lisp
+++ src/stream.lisp
@@ -1,6 +1,7 @@
-;;; stream.lisp ---
+;;; stream.lisp —
 
-;; copyright (C) 2012, 2013 David Vazquez Copyright (C) 2012 Raimon Grau
+;; Copyright © 2012, 2013 David Vazquez 
+;;; Copyright © 2012 Raimon Grau
 
 ;; JSCL is free software: you can redistribute it and/or modify it under
 ;; the terms of the GNU General  Public License as published by the Free
@@ -15,43 +16,58 @@
 ;; You should  have received a  copy of  the GNU General  Public License
 ;; along with JSCL. If not, see <http://www.gnu.org/licenses/>.
 
-(in-package :jscl) #-jscl-xc #.(error "Do not load this file in the host compiler")
+(in-package :jscl)
 
 ;;; TODO:   Use  structures   to   represent  streams,   but  we   would
 ;;; need inheritance.
 
 (/debug "loading stream.lisp!")
 
-(defvar *standard-output*)
+(defvar jscl/cl::*standard-output*)
 
-(defun streamp (x)
-  (and (vectorp x) (eq (aref x 0) 'stream)))
+(defun jscl/cl::streamp (x)
+  (and (storage-vector-p x) 
+       (subtypep (car (storage-vector-kind x)) 'stream)))
 
-(defun write-char (char &optional (stream *standard-output*))
-  (funcall (aref stream 1) char))
+(defun jscl/cl::output-stream-p (x)
+  (and (streamp x) 
+       (find (second (storage-vector-kind x)) '(output i/o))))
 
-(defun write-string (string &optional (stream *standard-output*))
-  (funcall (aref stream 2) string))
+(defvar *stream-generic-functions*
+  (cons 'string-stream
+        (list 'write-char 
+              (lambda (char stream)
+                (vector-push-extend ch buffer))
+              'write-string 
+              (lambda (string stream)
+                (dotimes (i (length string))
+                  (vector-push-extend (aref string i) buffer))))))
 
+(defun stream-generic-method (stream method)
+  (or (getf (or (getf *stream-generic-functions*
+                      (car (storage-vector-kind stream)))
+                (error "Stream class ~s has no methods"
+                       (car (storage-vector-kind stream))))
+            method)
+      (error "Stream class ~s has no method ~s"
+             (car (storage-vector-kind stream)) method)))
 
-(defun make-string-output-stream ()
-  (let ((buffer (make-string 0)))
-    (vector 'stream
-            ;; write-char
-            (lambda (ch)
-              (vector-push-extend ch buffer))
-            (lambda (string)
-              (dotimes (i (length string))
-                (vector-push-extend (aref string i) buffer)))
-            'string-stream
-            buffer)))
+(defun jscl/cl::write-char (char &optional (stream *standard-output*)) 
+  (assert (jscl/cl::output-stream-p stream))
+  (funcall (stream-generic-method stream 'write-char) char stream))
 
-(defun get-output-stream-string (stream)
-  (eq (aref stream 3) 'string-stream)
-  (prog1 (aref stream 4)
-    (aset stream 4 (make-string 0))))
+(defun jscl/cl::write-string (string &optional (stream *standard-output*))
+  (assert (jscl/cl::output-stream-p stream))
+  (funcall (stream-generic-method stream 'write-string) string stream))
 
-(defmacro with-output-to-string ((var) &body body)
+(defun jscl/cl::make-string-output-stream ()
+  (make-storage-vector 0 '(string-stream output)))
+
+(defun jscl/cl::get-output-stream-string (stream)
+  (assert (eq (car (storage-vector-kind stream)) 'string-stream))
+  (copy-seq (storage-vector-underlying-vector)))
+
+(defmacro jscl/cl::with-output-to-string ((var) &body body)
   `(let ((,var (make-string-output-stream)))
      ,@body
      (get-output-stream-string ,var)))
