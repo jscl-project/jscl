e1d622e758538afe6e8794fc3cbcca0e0fad29ab
David Vázquez Púa <davazp@gmail.com>
2018-02-11 13:50:57 +0100

    Add some test cases

--- tests/format.lisp
+++ tests/format.lisp
@@ -14,3 +14,9 @@ a" (format nil "a~%a")))
 (test (string= "this is \"foo\"" (format nil "this is ~S" "foo")))
 
 (test (string= "this is 2" (format nil "this is ~*~A" 1 2)))
+
+;;; ~C
+(test (string= "a" (format nil "~C" #\a)))
+(test (string= " " (format nil "~C" #\space)))
+(test (string= "Space" (format nil "~:C" #\space)))
+(test (string= "Newline" (format nil "~:C" #\newline)))
