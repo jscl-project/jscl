a18cd4709b2d0c2f6e33a3e43559dd3a698be941
David Vázquez Púa <davazp@gmail.com>
2018-04-21 12:22:22 +0200

    Add test for (SETF AREF)

--- tests/array.lisp
+++ tests/array.lisp
@@ -2,3 +2,8 @@
 (test (arrayp #(1 2 3 4)))
 (test (vectorp #(1 2 3 4)))
 (test (not (vectorp (make-array '(3 3)))))
+
+(let ((array #(1 2 3 4)))
+  (setf (aref array 0) t)
+  (test (eq (aref array 0) t)))
+
