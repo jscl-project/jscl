fbd8cb5bd0351ef9db08f22961195375c339e574
David Vázquez Púa <davazp@gmail.com>
2018-12-15 12:31:15 +0100

    Add a test case for issue #341

---
+++ tests/multiple-values.lisp
@@ -0,0 +1,10 @@
+;; Regression for issue
+;;   https://github.com/jscl-project/jscl/issues/341
+;;
+;; This is written in a block/return in an attempt to isolate the
+;; problem from the context of the `test` macro:
+(defun test-progn-multiple-value ()
+  (block nil
+    (return (eq 42 (progn nil (values 42))))))
+
+(test (test-progn-multiple-value))
