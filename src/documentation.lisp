1f65040167b2359c451b0b856225fb364baa97d6
vlad-km <gm5461@gmail.com>
2018-12-22 11:56:54 +0300

    Performance - change 'apropos' function

--- src/documentation.lisp
+++ src/documentation.lisp
@@ -48,16 +48,22 @@
         (map-for-in single-package *package-table*))
     result))
 
-(defun apropos (string &optional package external-only)
-  (map-apropos-symbols
-   (lambda (symbol)
-     (format t "~S" symbol)
-     (when (boundp symbol)
-       (format t " (bound)"))
-     (when (fboundp symbol)
-       (format t " (fbound)"))
-     (terpri))
-   (string string) package external-only))
+(defun apropos (string &optional package externals-only)
+  (princ
+   (with-output-to-string (buf)
+     (dolist (it (apropos-list string  package externals-only))
+       (prin1 it buf)
+       (when (boundp it)
+         (princ ",  Value: " buf)
+         (prin1 (symbol-value it) buf))
+       (when (fboundp it)
+         (princ ",  Def: " buf)
+         (if (find-generic-function it nil)
+             (princ "Standard generic function" buf)
+             (princ "Function" buf)))
+       (format buf "~%"))))
+  (terpri)
+  (values))
 
 ;;; DESCRIBE
 
