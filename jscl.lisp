b4fb4f1f90244abd8b7202de06c92085834a62a0
Yoteichi <plonk@piano.email.ne.jp>
2019-01-02 05:38:13 +0900

    read-whole-file: Trim array to character count

--- jscl.lisp
+++ jscl.lisp
@@ -145,9 +145,10 @@
 
 (defun read-whole-file (filename)
   (with-open-file (in filename)
-    (let ((seq (make-array (file-length in) :element-type 'character)))
-      (read-sequence seq in)
-      (substitute-if #\Space #'(lambda (char) (eql char #\Return)) seq))))
+    (let* ((seq (make-array (file-length in) :element-type 'character))
+           (char-count (read-sequence seq in))
+           (seq1 (substitute-if #\Space #'(lambda (char) (eql char #\Return)) seq)))
+      (subseq seq1 0 char-count))))
 
 (defun !compile-file (filename out &key print)
   (let ((*compiling-file* t)
