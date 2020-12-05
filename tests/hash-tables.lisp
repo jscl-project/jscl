55c37877ad75eff37ab797c42c76371a3bb535fc
vlad <26150554+vlad-km@users.noreply.github.com>
2020-12-05 13:25:28 +0300

    Add test cases

--- tests/hash-tables.lisp
+++ tests/hash-tables.lisp
@@ -63,3 +63,70 @@
    (eq nil (maphash (lambda (key value)
 		      (declare (ignore key value)))
 		    ht))))
+(test (eq (hash-table-count (make-hash-table)) 0))
+
+(defun temp-hash-table (&key test (fill nil))
+  (let ((h (apply 'make-hash-table (if test (list :test test))))
+        (keys '(one two three four five six))
+        (vals '(1 2 3 4  5 6)))
+    (if fill
+        (mapcar (lambda (x y) (setf (gethash x h) y))
+                keys
+                vals))
+    h))
+
+(test (eq (hash-table-count (temp-hash-table)) 0))
+(test (eq (hash-table-count (temp-hash-table :fill t)) 6))
+
+;;; test clrhash
+(test
+ (eq
+  (let ((h (temp-hash-table :fill t)))
+    (clrhash h)
+    (hash-table-count h))
+  0))
+
+;;; test copy-hash
+(test
+ (equal 
+  (let* ((ht (temp-hash-table :fill t))
+         (ht-copy (jscl::copy-hash-table ht))
+         (map nil))
+    (maphash
+     (lambda (k1 v1)
+       (let ((v2 (gethash k1 ht-copy)))
+         (push (eq v1 v2) map))) ht)
+    (remove-duplicates map))
+  '(t)))
+
+(test
+ (equal
+  (let* ((ht (temp-hash-table :test #'equal :fill t))
+         (ht-copy)
+         (map nil))
+    (map nil (lambda (key)
+               (setf (gethash key ht) (if (symbolp key)
+                                          (symbol-name key)
+                                          key)))
+         '(one-key two-key set-difference multiple-value-bind :test "small string"))
+    (setq ht-copy (jscl::copy-hash-table ht))
+    (maphash
+     (lambda (k1 v1)
+       (let ((v2 (gethash k1 ht-copy)))
+         (push (equal v1 v2) map))) ht)
+    (remove-duplicates map))
+  '(t)))
+
+;;; test hash-table-printer
+(test (string= "#<hash-table :test eq :count 0>"
+               (write-to-string (temp-hash-table :test #'eq))))
+(test (string= "#<hash-table :test eql :count 0>"
+               (write-to-string (temp-hash-table))))
+(test (string= "#<hash-table :test equal :count 0>"
+               (write-to-string (temp-hash-table :test #'equal))))
+(test (string= "#<hash-table :test eql :count 6>"  (write-to-string (temp-hash-table :fill t))))
+(test (string= "#<hash-table :test eql :count 5>"
+               (write-to-string
+                (let ((h (temp-hash-table :fill t)))
+                  (remhash 'one h)
+                  h))))
