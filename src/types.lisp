3e0ccac6b9e9cf3c3ef01820b7f0df382fdfdea1
vlad <26150554+vlad-km@users.noreply.github.com>
2021-03-05 11:43:45 +0300

    Create types.lisp

---
+++ src/types.lisp
@@ -0,0 +1,16 @@
+;;; -*- mode:lisp; coding:utf-8 -*-
+
+;;; DEFTYPE
+(/debug "loading types.lisp!")
+
+
+(defvar *types* (make-hash-table :test #'eq))
+
+(defvar *class-table* (make-hash-table :test #'eq))
+
+(defvar *structures* (make-hash-table :test #'eq))
+
+
+
+;;; EOF
+
