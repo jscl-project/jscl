ed1b71bc6dd3822985bd373fd82ee399d5f0ab80
vlad <26150554+vlad-km@users.noreply.github.com>
2021-03-13 06:12:08 +0300

    tests multiple-values revision

--- tests/multiple-values.lisp
+++ tests/multiple-values.lisp
@@ -1,10 +1,16 @@
-;; Regression for issue
-;;   https://github.com/jscl-project/jscl/issues/341
-;;
-;; This is written in a block/return in an attempt to isolate the
-;; problem from the context of the `test` macro:
+;;; -*- mode:lisp; coding:utf-8 -*-
+
+(/debug "perform test/multiple-values.lisp!")
+
+;;; Regression for issue
+;;;   https://github.com/jscl-project/jscl/issues/341
+;;;
+;;; This is written in a block/return in an attempt to isolate the
+;;; problem from the context of the `test` macro:
 (defun test-progn-multiple-value ()
   (block nil
     (return (eq 42 (progn nil (values 42))))))
 
 (test (test-progn-multiple-value))
+
+;;; EOF
