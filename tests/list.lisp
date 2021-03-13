b7d7428a4698cce84325634c07f91115b4a147f0
vlad <26150554+vlad-km@users.noreply.github.com>
2021-03-13 06:10:15 +0300

    tests list revision

--- tests/list.lisp
+++ tests/list.lisp
@@ -1,6 +1,10 @@
-;; Tests for list functions
+;;; -*- mode:lisp; coding:utf-8 -*-
 
-;; CONS
+(/debug "perform test/list.lisp!")
+
+;;; Tests for list functions
+
+;;; CONS
 (test (equal (cons 1 2) '(1 . 2)))
 (test (equal (cons 1 nil) '(1)))
 (test (equal (cons nil 2) '(NIL . 2)))
@@ -10,31 +14,31 @@
 (test (equal (cons 'a (cons 'b (cons 'c '()))) '(A B C)))
 (test (equal (cons 'a '(b c d)) '(A B C D)))
 
-;; CONSP
+;;; CONSP
 (test (not (consp 'nil)))
 (test (not (consp nil)))
 (test (not (consp ())))
 (test (not (consp '())))
 (test (consp (cons 1 2)))
 
-;; ATOM
+;;; ATOM
 (test (atom 'sss))
 (test (not (atom (cons 1 2))))
 (test (atom nil))
 (test (atom '()))
 (test (atom 3))
 
-;; RPLACA
+;;; RPLACA
 (let ((some-list (list* 'one 'two 'three 'four)))
   (test (equal (rplaca some-list 'uno) '(UNO TWO THREE . FOUR)))
   (test (equal some-list '(UNO TWO THREE . FOUR))))
 
-;; RPLACD
+;;; RPLACD
 (let ((some-list (list* 'one 'two 'three 'four)))
   (test (equal (rplacd (last some-list) (list 'IV)) '(THREE IV)))
   (test (equal some-list '(ONE TWO THREE IV))))
 
-;; CAR, CDR and variants
+;;; CAR, CDR and variants
 (test (equal (car nil) nil))
 (test (equal (cdr '(1 . 2)) 2))
 (test (equal (cdr '(1 2)) '(2)))
@@ -70,7 +74,7 @@
 (test (equal (cdddar '((1 2 3 4))) '(4)))
 (test (equal (cddddr '(1 2 3 4 5)) '(5)))
 
-;; SUBLIS
+;;; SUBLIS
 (test (equal (sublis '((x . 100) (z . zprime))
                      '(plus x (minus g z x p) 4 . x))
              '(PLUS 100 (MINUS G ZPRIME 100 P) 4 . 100)))
@@ -93,7 +97,7 @@
   (test (equal (sublis '(("two" . 2)) tree2 :test 'equal)
                '("one" ("one" 2) (("one" "Two" "three"))))))
 
-;; SUBST
+;;; SUBST
 (let ((tree1 '(1 (1 2) (1 2 3) (1 2 3 4))))
   (test (equal (subst "two" 2 tree1) '(1 (1 "two") (1 "two" 3) (1 "two" 3 4))))
   (test (equal (subst "five" 5 tree1) '(1 (1 2) (1 2 3) (1 2 3 4))))
@@ -109,12 +113,12 @@
                     :test #'equal)
              '((OLD . SPICE) ((OLD . SHOES) A . CONS) (A . CONS))))
 
-; COPY-LIST
+;;; COPY-LIST
 (test (eql   (copy-list nil)          nil))
 (test (equal (copy-list (list nil))   (list nil)))
 (test (equal (copy-list (list 1 2 3)) (list 1 2 3)))
 
-; COPY-TREE
+;;; COPY-TREE
 (test (let* ((foo (list '(1 2) '(3 4)))
              (bar (copy-tree foo)))
         ;; (SETF (CAR (CAR FOO)) 0) doesn't work in the test for some reason,
@@ -123,7 +127,7 @@
         (not (= (car (car foo))
                 (car (car bar))))))
 
-; TREE-EQUAL
+;;; TREE-EQUAL
 (test (tree-equal '(1 2 3) '(1 2 3)))
 (test (not (tree-equal '(1 2 3) '(3 2 1))))
 (test (tree-equal '(1 (2 (3 4) 5) 6) '(1 (2 (3 4) 5) 6)))
@@ -131,7 +135,7 @@
 (test (tree-equal '(1 . 2) '(2 . 1) :test-not #'eql))
 (test (not (tree-equal '(1 . 2) '(1 . 2) :test-not #'eql)))
 
-; FIRST to TENTH
+;;; FIRST to TENTH
 (let ((nums '(1 2 3 4 5 6 7 8 9 10)))
   (test (= (first   nums) 1))
   (test (= (second  nums) 2))
@@ -144,32 +148,32 @@
   (test (= (ninth   nums) 9))
   (test (= (tenth   nums) 10)))
 
-; TAILP
+;;; TAILP
 (let* ((a (list 1 2 3))
        (b (cdr a)))
   (test (tailp b a))
   (test (tailp a a)))
 (test (tailp 'a (cons 'b 'a)))
 
-; ACONS
+;;; ACONS
 (test (equal '((1 . 2) (3 . 4))
              (acons 1 2 '((3 . 4)))))
 (test (equal '((1 . 2)) (acons 1 2 ())))
 
-; PAIRLIS
+;;; PAIRLIS
 (test (equal '((1 . 3) (0 . 2))
              (pairlis '(0 1) '(2 3))))
 (test (equal '((1 . 2) (a . b))
              (pairlis '(1) '(2) '((a . b)))))
 
-; COPY-ALIST
+;;; COPY-ALIST
 (let* ((alist '((1 . 2) (3 . 4)))
        (copy (copy-alist alist)))
   (test (not (eql alist copy)))
   (test (not (eql (car alist) (car copy))))
   (test (equal alist copy)))
 
-; ASSOC and RASSOC
+;;; ASSOC and RASSOC
 (let ((alist '((1 . 2) (3 . 4))))
   (test (equal (assoc  1 alist) '(1 . 2)))
   (test (equal (rassoc 2 alist) '(1 . 2)))
@@ -180,19 +184,19 @@
   (test (equal (assoc  1 alist :key (lambda (x) (/ x 3))) '(3 . 4)))
   (test (equal (rassoc 2 alist :key (lambda (x) (/ x 2))) '(3 . 4)))) 
 
-; MEMBER
+;;; MEMBER
 (test (equal (member 2 '(1 2 3)) '(2 3)))
 (test (not   (member 4 '(1 2 3))))
 (test (equal (member 4 '((1 . 2) (3 . 4)) :key #'cdr) '((3 . 4))))
 (test (member '(2) '((1) (2) (3)) :test #'equal))
 (test (member 1 '(1 2 3) :test-not #'eql))
 
-; ADJOIN
+;;; ADJOIN
 (test (equal (adjoin 1 '(2 3))   '(1 2 3)))
 (test (equal (adjoin 1 '(1 2 3)) '(1 2 3)))
 (test (equal (adjoin '(1) '((1) (2)) :test #'equal) '((1) (2))))
 
-; INTERSECTION
+;;; INTERSECTION
 (test (equal (intersection '(1 2) '(2 3)) '(2)))
 (test (not (intersection '(1 2 3) '(4 5 6))))
 (test (equal (intersection '((1) (2)) '((2) (3)) :test #'equal) '((2))))
@@ -200,16 +204,16 @@
              (intersection '((1 . 2) (2 . 3)) '((9 . 2) (9 . 4))
                            :test #'equal :key #'cdr)))
 
-; POP
+;;; POP
 (test (let* ((foo '(1 2 3))
              (bar (pop foo)))
         (and (= bar 1)
              (= (car foo) 2))))
 
-;; MAPCAR
+;;;; MAPCAR
 (test (equal (mapcar #'+ '(1 2) '(3) '(4 5 6)) '(8)))
 
-;; MAPLIST
+;;;; MAPLIST
 (test (equal '((1 2 3 4 1 2 1 2 3) (2 3 4 2 2 3))
 	     (maplist #'append '(1 2 3 4) '(1 2) '(1 2 3))))
 (test (equal '((FOO A B C D) (FOO B C D) (FOO C D) (FOO D))
@@ -217,24 +221,24 @@
 (test (equal '(0 0 1 0 1 1 1)
 	     (maplist #'(lambda (x) (if (member (car x) (cdr x)) 0 1)) '(a b a c d b c))))
 
-;; MAPC
+;;;; MAPC
 (test (equal (mapc #'+ '(1 2) '(3) '(4 5 6)) '(1 2)))
 (test (let (foo)
         (mapc (lambda (x y z) (push (+ x y z) foo)) '(1 2) '(3) '(4 5 6))
         (equal foo '(8))))
 
-;; GETF
+;;;; GETF
 (test (eq (getf '(a b c d) 'a) 'b))
 (test (null (getf '(a b c d) 'e)))
 (test (equal (let ((x (list 'a 1))) (setf (getf x 'a) 3) x) '(a 3)))
 (test (equal (let ((x (list 'a 1))) (incf (getf x 'a)) x) '(a 2)))
 
-;; GET-PROPERTIES
+;;; GET-PROPERTIES
 (test (equal (multiple-value-list (get-properties '(a b c d) '(b d e))) '(NIL NIL NIL)))
 (test (equal (multiple-value-list (get-properties '(a b c d) '(b a c))) '(a b (a b c d))))
 (test (equal (multiple-value-list (get-properties '(a b c d) '(b c a))) '(a b (a b c d))))
 
-;; BUTLAST
+;;; BUTLAST
 (test (equal (butlast '()) ()))
 (test (equal (butlast '(1)) ()))
 (test (equal (butlast '(1 2)) '(1)))
@@ -250,7 +254,7 @@
 (test (equal (let ((thing '(1 2))) (butlast thing 2)) '()))
 (test (equal (let ((thing '())) (butlast thing 0)) '()))
 
-;; MAKE-LIST
+;;; MAKE-LIST
 (test (equal (make-list 5) '(nil nil nil nil nil)))
 (test (equal (make-list 3 :initial-element 'rah) '(rah rah rah)))
 
@@ -266,7 +270,7 @@
            (list t t t)))) 
 
 
-;; SORT
+;;; SORT
 #+jscl
 (test 
  (equal (apply 'jscl::concat
@@ -279,7 +283,6 @@
                       (sort (jscl::vector-to-list "cdbaxaybzcd") #'char-lessp))))
      (equal (remove-duplicates sorted :test #'char-equal :from-end t) "abcdxyz")))
 
-
 (test 
  (equal (sort '((1 2 3) (4 5 6) (7 8 9))  #'> :key #'car)
         '((7 8 9) (4 5 6) (1 2 3))))
@@ -290,7 +293,6 @@
 (test
  (equal (union '(a b c) '(f a d))
         '(C B F A D)))
-
 
 (test 
  (equal (union '((x 5) (y 6)) '((z 2) (x 4) (z 2)) 
@@ -306,10 +308,11 @@
             '("b" "a" (1 2) 1 2 3 (2 3) "B" "C"))))
 
 
-;; See https://github.com/jscl-project/jscl/issues/369
+;;; See https://github.com/jscl-project/jscl/issues/369
 (test
  (equal
   (let ((x (cons 'a 'b))
         b)
     (rplacd x (progn (setq b 0) 'c)))
   '(a . c )))
+;;; EOF
