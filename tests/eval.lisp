e77c4802c68e67e68fce9b8c71319315120d02ee
vlad <26150554+vlad-km@users.noreply.github.com>
2021-03-13 06:08:30 +0300

    tests eval revision

--- tests/eval.lisp
+++ tests/eval.lisp
@@ -1,5 +1,11 @@
+;;; -*- mode:lisp; coding:utf-8 -*-
+
+(/debug "perform test/eval.lisp!")
+
+
 (test (= (eval '(+ 1 2)) 3))
 (test (= 42 (progn
               (eval '(defun test-fun (x y)
                       (+ x y)))
               (eval '(test-fun 40 2)))))
+;;; EOF
