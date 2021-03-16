d6504575d6dc8beac78721e56ce7e8be85614f7a
vlad <26150554+vlad-km@users.noreply.github.com>
2021-03-16 23:40:33 +0300

    Update types.lisp

--- src/types.lisp
+++ src/types.lisp
@@ -1,7 +1,20 @@
 ;;; -*- mode:lisp; coding:utf-8 -*-
 
-;;; Tiny type system for JSCL
-;;; @vkm
+;;; Tiny type system for JSCL  @vkm
+;;;
+;;; JSCL is free software: you can redistribute it and/or
+;;; modify it under the terms of the GNU General Public License as
+;;; published by the Free Software Foundation, either version 3 of the
+;;; License, or (at your option) any later version.
+;;;
+;;; JSCL is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with JSCL.  If not, see <http://www.gnu.org/licenses/>.
+
 (/debug "loading types.lisp!")
 
 ;;; DEFTYPE
@@ -17,7 +30,6 @@
     (while list
            (if (eq item (car list)) (return list))
            (setq list (cdr list)))))
-
 
 ;;; lightweight mapcar
 (eval-when (:compile-toplevel :load-toplevel :execute)
