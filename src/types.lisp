c3058e0c02039e553f843eb4ea66f77ef03e60ec
vlad <26150554+vlad-km@users.noreply.github.com>
2021-05-06 10:06:45 +0300

    Update types.lisp
    for :both compilation mode

--- src/types.lisp
+++ src/types.lisp
@@ -17,13 +17,6 @@
 
 (/debug "loading types.lisp!")
 
-;;; DEFTYPE
-(defvar *types* (make-hash-table :test #'eq))
-;;; DEFSTRUCT
-(defvar *structures* (make-hash-table :test #'eq))
-;;; DEFCLASS
-(defvar *class-table* (make-hash-table :test #'eq))
-
 ;;; for internal used only
 (defun true () t)
 (defun false () nil)
@@ -54,83 +47,6 @@
 ;;; todo: rename true-list-p -> proper-list-p
 ;;;       move to list.lisp
 (defun true-list-p (obj) (and (consp obj) (not (true-cons-p obj))))
-
-;;; another member from ccl
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (defun memq (item list)
-    (while list
-           (if (eq item (car list)) (return list))
-           (setq list (cdr list)))))
-
-;;; lightweight mapcar
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (defun %lmapcar (fn list)
-    (let* ((ret-list (list nil))
-           (temp ret-list)
-           (res nil))
-      (while list
-        (setq res (funcall fn (car list))
-              list (cdr list))
-        (setf (cdr temp) (list res)
-              temp (cdr temp)))
-      (cdr ret-list)))
-  )
-
-;;; internal unsafe defstruct version
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (defun %struct-generator (kind options slots)
-    (let* ((constructor (cadr (assoc :constructor options)))
-           (keys (cadr (assoc :form options)))
-           (names (mapcar (lambda (x) (if (consp x) (car x) x)) slots))
-           (option (if keys keys '&optional))
-           (maker)
-           (makname (if constructor
-                        (intern (symbol-name constructor))
-                        (intern (concat "%MAKE-" (symbol-name `,kind)))))
-           (getter)
-           (position 0)
-           (q))
-      (unless (memq option '(&key &optional)) (error "Something went wrong: ~a." options))
-      (setq maker
-            `(defun ,makname (,option ,@slots)  (list ,@names)))
-      (dolist (it names)
-        (setq getter (intern (concat (symbol-name kind) "-" (symbol-name it))))
-        (push `(defun ,getter (storage)(nth ,position storage)) q)
-        (push `(defun (setf ,getter) (value storage)
-                 (rplaca (nthcdr ,position storage) value) value)
-              q)
-        (incf position))
-      (values maker (reverse q)))))
-
-(defmacro %i-struct (name-options &rest slots)
-  (let* ((name-options (ensure-list name-options))
-         (name (car name-options))
-         (options (rest name-options)))
-    (multiple-value-bind (maker accessors) (%struct-generator name options slots)
-      `(progn
-         ,maker
-         ,@accessors))))
-
-(%i-struct (type-info (:form &key)) name expand compound predicate)
-
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (defun %deftype-info (name &optional (create t))
-    (unless (symbolp name) (error "Not a symbol ~a." name))
-    (let ((exists (gethash name *types*)))
-      (cond ((and (not exists) create)
-             (setq exists (%make-type-info :name name))
-             (setf (gethash name *types*) exists))
-            (t exists)))))
-
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (defun %deftype (name &key expander compound predicate)
-    (let ((type (%deftype-info name)))
-      (when expander
-        (setf (type-info-expand type) expander) )
-      (when compound
-        (setf (type-info-compound type) compound))
-      (when predicate
-        (setf (type-info-predicate type) predicate)))))
 
 (defun get-expander-for (type)
   (let ((exists (%deftype-info (if (symbolp type) type (car type)) nil)))
