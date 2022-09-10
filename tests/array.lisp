56e11c1c93df27ede354f54de0e0fb2be2770ab6
vlad-km <gm5461@gmail.com>
2022-09-10 15:34:16 +0300

    add test cases

--- tests/array.lisp
+++ tests/array.lisp
@@ -15,4 +15,29 @@
   (setf (fill-pointer vector) 20)
   (test (= (length vector) 20)))
 
+(test
+ (mv-eql
+  (let ((vector (make-array 10 :fill-pointer 1))
+        (new-idx))
+    (values
+     (dotimes (i 10 new-idx)
+       (push (vector-push i vector) new-idx))
+     (vector-to-list vector)))
+  (NIL 9 8 7 6 5 4 3 2 1)
+  (NIL 0 1 2 3 4 5 6 7 8)))
+
+;;; from clhs.lisp.se
+(test
+ (mv-eql
+  (let (fable fa)
+    (values
+     (vector-push (setq fable (list 'fable))
+                  (setq fa (make-array 8 
+                                       :fill-pointer 2
+                                       :initial-element 'first-one)))
+     (fill-pointer fa)
+     (eq (aref fa 2) fable)))
+  2 3 t))
+
+
 ;;; EOF
