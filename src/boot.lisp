c06586a05b5f3684b184b0bfcdec4302d97ea2fb
vlad-km <gm5461@gmail.com>
2022-11-27 14:20:51 +0300

    New FFI predicates
    added to the type system
    
    js-null-p
    js-undefined-p

--- src/boot.lisp
+++ src/boot.lisp
@@ -496,6 +496,7 @@
   (defparameter *basic-type-predicates*
     '((hash-table . hash-table-p) (package . packagep) (stream . streamp)
       (atom . atom) (structure . structure-p) (js-object . js-object-p)
+      (js-null . js-null-p) (js-undefined . js-undefined-p)
       ;; todo: subtypep - remove mop-object from tables
       (clos-object . mop-object-p) (mop-object . mop-object-p) (character . characterp)
       (symbol . symbolp)  (keyword . keywordp)
