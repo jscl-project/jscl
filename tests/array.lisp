72fdf7ec829728a0f0f0e6bf89d8881424a15226
Qiantan Hong <qthong@stanford.edu>
2025-11-13 12:17:50 -0800

    test compliance: don't mutate literal lists/vectors

--- tests/array.lisp
+++ tests/array.lisp
@@ -6,7 +6,7 @@
 (test (vectorp #(1 2 3 4)))
 (test (not (vectorp (make-array '(3 3)))))
 
-(let ((array #(1 2 3 4)))
+(let ((array (vector 1 2 3 4)))
   (setf (aref array 0) t)
   (test (eq (aref array 0) t)))
 
