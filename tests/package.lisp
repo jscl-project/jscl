c66a82f688a62e3860b66cbff9dc6a2f417b4ce3
Qiantan Hong <qthong@stanford.edu>
2025-11-19 14:46:20 -0800

    clean up delete-package in package tests

--- tests/package.lisp
+++ tests/package.lisp
@@ -21,8 +21,16 @@
 
 (make-package 'fubar)
 
-(when (find-package 'foo)
-     (delete-package (find-package 'foo)))
+(defun delete-test-packages ()
+  ;; Delete in reverse order of possible USE dependency
+  (when (find-package 'baz)
+    (delete-package (find-package 'baz)))
+  (when (find-package 'bar)
+    (delete-package (find-package 'bar)))
+  (when (find-package 'foo)
+    (delete-package (find-package 'foo))))
+
+(delete-test-packages)
 (test
  (let ((package (make-package 'foo :use '(cl)))
        foo-symbols
@@ -34,8 +42,7 @@
    (and (not (null foo-symbols))
         (equal foo-symbols cl-symbols))))
 
-(when (find-package 'bar)
-   (delete-package (find-package 'bar)))
+(delete-test-packages)
 (test
  (let* ((package (make-package 'bar))
         (baz (intern (string 'baz) package)))
@@ -45,12 +52,7 @@
      (and (member 'car symbols)
           (member baz symbols)))))
 
-(when (find-package 'foo)
-  (delete-package (find-package 'foo)))
-(when (find-package 'bar)
-  (delete-package (find-package 'bar)))
-(when (find-package 'baz)
-  (delete-package (find-package 'baz)))
+(delete-test-packages)
 (test
  (let* ((package (make-package 'foo :nicknames '(bar baz)))
         (symbol (intern (string 'foo) package))
@@ -65,8 +67,7 @@
         (member symbol baz-symbols))))
 
 ;;; (UN)USE-PACKAGE
-(when (find-package 'foo)
-  (delete-package (find-package 'foo)))
+(delete-test-packages)
 (test (let ((package (make-package 'foo :use '(cl))))
         (and (eq (find-symbol (string 'car) package) 'cl::car)
              (unuse-package 'cl package)
@@ -74,10 +75,7 @@
              (use-package 'cl package)
              (eq (find-symbol (string 'car) package) 'cl::car))))
 
-(when (find-package 'foo)
-  (delete-package (find-package 'foo)))
-(when (find-package 'bar)
-  (delete-package (find-package 'bar)))
+(delete-test-packages)
 (test (let ((foo (make-package 'foo :use '(cl)))
             (bar (make-package 'bar :use '(foo))))
         (and (export 'cl:cdr foo)
@@ -86,10 +84,7 @@
              (eq (package-used-by-list foo) nil))))
 
 ;;; UNEXPORT
-(when (find-package 'foo)
-  (delete-package (find-package 'foo)))
-(when (find-package 'bar)
-  (delete-package (find-package 'bar)))
+(delete-test-packages)
 (test
  (let* ((foo (make-package 'foo))
         (bar (make-package 'bar))
@@ -101,10 +96,7 @@
         (eq (find-symbol (string 'foo) bar) nil))))
 
 ;;; UNINTERN
-(when (find-package 'foo)
-  (delete-package (find-package 'foo)))
-(when (find-package 'bar)
-  (delete-package (find-package 'bar)))
+(delete-test-packages)
 (test
  (let* ((foo (make-package 'foo))
         (bar (make-package 'bar))
@@ -117,18 +109,14 @@
         (eq (find-symbol (string 'foo) bar) nil))))
 
 ;;; EXPORT should not export inaccessible symbol
-(when (find-package 'foo)
-  (delete-package (find-package 'foo)))
+(delete-test-packages)
 (test
  (let ((foo (make-package 'foo)))
    (and (not (ignore-errors (export 'cl:car foo) t))
         (eq (find-symbol (string 'car) foo) nil))))
 
 ;;; SHADOWING-IMPORT
-(when (find-package 'foo)
-  (delete-package (find-package 'foo)))
-(when (find-package 'bar)
-  (delete-package (find-package 'bar)))
+(delete-test-packages)
 (test
  (let* ((foo (make-package 'foo))
         (bar (make-package 'bar :use '(cl)))
