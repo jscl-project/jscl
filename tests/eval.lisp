a1fe7218d1068ac4784b8030179ef65ac4c72c0a
Qiantan Hong <qthong@stanford.edu>
2025-12-10 02:38:50 -0800

    Use constant vector to pass literal objects in EVAL, close #527

--- tests/eval.lisp
+++ tests/eval.lisp
@@ -8,4 +8,19 @@
               (eval '(defun test-fun (x y)
                       (+ x y)))
               (eval '(test-fun 40 2)))))
+
+
+;;; CLHS 3.2.4.4
+(test (let ((name (gensym)))
+        (eq name (eval `',name))))
+
+(test (let* ((name (gensym))
+             (list (eval `'(,name ,name))))
+        (eq (car list) (cadr list))))
+
+(test (equal "FOO"
+             (let ((name (gensym)))
+               (eval `(defvar ,name "FOO"))
+               (symbol-value name))))
+
 ;;; EOF
