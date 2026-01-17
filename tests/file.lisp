f1dd1e13c27a1b5fbd5e388ab063647ba3a8d7e8
Qiantan Hong <qthong@stanford.edu>
2026-01-16 19:11:27 -0800

    use more widely supported mkdtempSync for test

--- tests/file.lisp
+++ tests/file.lisp
@@ -1,17 +1,17 @@
 ;;; -*- mode:lisp; coding:utf-8 -*-
 (/debug "perform test/file.lisp!")
 
-(defvar *tmp-dir* (#j:Fs:mkdtempDisposableSync "JSCL_TEST"))
+(defvar *tmp-dir* (#j:Fs:mkdtempSync "JSCL_TEST"))
 
 (test
  (progn
-   (with-open-file (s (concat (oget *tmp-dir* "path") "file-1.txt")
+   (with-open-file (s (concat *tmp-dir* "file-1.txt")
                       :direction :output :if-exists :supersede)
      (write-line "abc" s)
      (write-line "foo" s))
-   (with-open-file (s (concat (oget *tmp-dir* "path") "file-1.txt"))
+   (with-open-file (s (concat *tmp-dir* "file-1.txt"))
      (and (equal (read-line s nil) "abc")
           (equal (read-line s nil) "foo")
           (equal (read-line s nil) nil)))))
 
-((oget *tmp-dir* "remove"))
+(test (probe-file (concat *tmp-dir* "file-1.txt")))
