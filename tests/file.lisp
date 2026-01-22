37a5be1277965dc807c07e857ed078910148be8d
Qiantan Hong <qthong@stanford.edu>
2026-01-22 00:23:11 -0800

    skip file tests on web

--- tests/file.lisp
+++ tests/file.lisp
@@ -1,18 +1,19 @@
 ;;; -*- mode:lisp; coding:utf-8 -*-
 (/debug "perform test/file.lisp!")
 
-(#j:Fs:mkdirSync ".jscl_test/" (new "recursive" t))
-(defvar *tmp-dir* (#j:Fs:mkdtempSync ".jscl_test/test-"))
+#+node
+(progn
+  (defvar *tmp-dir* (#j:Fs:mkdtempSync ".jscl_test/test-"))
 
-(test
- (progn
-   (with-open-file (s (concat *tmp-dir* "/file-1.txt")
-                      :direction :output :if-exists :supersede)
-     (write-line "abc" s)
-     (write-line "foo" s))
-   (with-open-file (s (concat *tmp-dir* "/file-1.txt"))
-     (and (equal (read-line s nil) "abc")
-          (equal (read-line s nil) "foo")
-          (equal (read-line s nil) nil)))))
+  (test
+   (progn
+     (with-open-file (s (concat *tmp-dir* "/file-1.txt")
+                        :direction :output :if-exists :supersede)
+       (write-line "abc" s)
+       (write-line "foo" s))
+     (with-open-file (s (concat *tmp-dir* "/file-1.txt"))
+       (and (equal (read-line s nil) "abc")
+            (equal (read-line s nil) "foo")
+            (equal (read-line s nil) nil)))))
 
-(test (probe-file (concat *tmp-dir* "/file-1.txt")))
+  (test (probe-file (concat *tmp-dir* "/file-1.txt"))))
