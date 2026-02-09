d9d0bd474382e530f747be05b36af837f9c67f1f
David Vázquez Púa <davazp@gmail.com>
2026-02-09 22:10:15 +0100

    Simplfication?

--- src/compiler/compiler.lisp
+++ src/compiler/compiler.lisp
@@ -578,11 +578,12 @@
 ;;; this symbol.
 (defvar *magic-unquote-marker* (gensym "MAGIC-UNQUOTE"))
 
-#-jscl-target
-(setf (macro-function *magic-unquote-marker*)
-      (lambda (form &optional environment)
-        (declare (ignore environment))
-        (second form)))
+;; TODO: I don't think this is needed? the magic unquote is only added during the dump-global-environment
+;; #-jscl-target
+;; (setf (macro-function *magic-unquote-marker*)
+;;       (lambda (form &optional environment)
+;;         (declare (ignore environment))
+;;         (second form)))
 
 (defvar *literal-table*)
 (defvar *literal-counter*)
