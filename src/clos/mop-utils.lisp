28a3b8ccf7fc77de9e34a99b2e49afe92e4f74c8
hemml <hemml@me.com>
2026-07-16 17:28:50 +0300

    Fix typo

--- src/clos/mop-utils.lisp
+++ src/clos/mop-utils.lisp
@@ -31,7 +31,7 @@
              (push "#(..) " result))
             ((or (numberp place)
                  (symbolp place))
-             (push (format nil "~A " place) result)
+             (push (format nil "~A " place) result))
             ((stringp place)
              (push (concat place " ") result))
             (t (push "@ " result))))
