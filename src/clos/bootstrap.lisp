896f66eb75ae004ec5b21a3d6587a9171475f4ac
Qiantan Hong <qthong@stanford.edu>
2026-07-27 14:49:42 -0700

    fix class precedence list for class T

--- src/clos/bootstrap.lisp
+++ src/clos/bootstrap.lisp
@@ -40,7 +40,7 @@
         (setf-class-direct-superclasses class ())
         (setf-class-direct-methods class ())
         (setf-class-direct-slots class ())
-        (setf-class-precedence-list class ())
+        (setf-class-precedence-list class (list class))
         (setf-class-slots class ())
         (hash-class-slots-names class)
         class))
